483b5b81e9e8fe5135d2c8616cf4b2e024319ac0
root <whqkrqkr12345@gmail.com>
2024-06-28 10:05:35 +0900

    Initial Commit

---
+++ SafeOrder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeOrder
+//
+//  Created by 조근호 on 6/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
